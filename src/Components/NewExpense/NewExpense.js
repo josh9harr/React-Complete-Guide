2bad04e86ea8cb8c1cd101384c10e43ae3648f2d
josh9harr <josh9harr@gmail.com>
2022-06-24 16:00:28 -0600

    Completed Leatchure 60

--- src/Components/NewExpense/NewExpense.js
+++ src/Components/NewExpense/NewExpense.js
@@ -1,13 +1,21 @@
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
-function NewExpense() {
+function NewExpense(props) {
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+        props.onAddExpense(expenseData);
+    };
 
-
-    return <div className="new-expense">
-            <ExpenseForm/>
+    return (
+        <div className="new-expense">
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
         </div>
-}
+    );
+};
 
 
 export default NewExpense;
