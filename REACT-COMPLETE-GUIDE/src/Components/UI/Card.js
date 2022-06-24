a7d1dbd6dafba0acfd06281570e09ffb4d026cdc
josh9harr <josh9harr@gmail.com>
2022-06-24 15:27:47 -0600

    Completed Lecture 56

--- REACT-COMPLETE-GUIDE/src/Components/UI/Card.js
+++
@@ -1,9 +0,0 @@
-import "Card.css";
-
-const Card = (props)  => {
-    const classes = 'card ' + props.className;
-    return <div className="card">{props.children}</div>
-}
-
-
-export default Card;
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Expenses from "./components/Expenses/Expenses";
-
+import Expenses from "./Components/Expenses/Expenses";
+import NewExpense from "./Components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -27,7 +27,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense/>
       <Expenses items={expenses} />
       
     </div>
